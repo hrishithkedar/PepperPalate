eb12911a0b340b9df61249c657bf5a28ff2fa2f9
hrishithkedar <hrishith2003@gmail.com>
2026-05-22 11:35:43 +0530

    Added timestmaps explicitly

--- pepper_backend/models/recipe.js
+++ pepper_backend/models/recipe.js
@@ -1,82 +1,79 @@
-const mongoose=require('mongoose')
-const Review=require('./review');
-const recipeSchema=new mongoose.Schema({
-    title:{
-        type:String,
-        required:true
-    },
-    imgSrc:{
-        type:String,
-    },
-    cookTime:{
-        type:String,
-        required:true
-    },
-    prepTime:{
-        type:String,
-        required:true
-    },
-    Servings:{
-        type:String,
-        required:true
-    },
-    cardDescription:{
-        type:String,
-        required:true
-    },
-    description:{
-        type:String,
-        required:true
-    },
-    sts_process:[
-        {
-            type:String,
-            required:true
-        }
-    ],
-    noOfIngredients:{
-        type:Number,
-        required:true
-    },
-    ingredients:[
-        {
-            type:String,
-            required:true
-        }
-    ],
-    nutrition:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:'Nutrition'
-    },
-    user:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:'User'
-    },
+const mongoose = require('mongoose');
+const Review = require('./review');
 
-    reviews:[
+const recipeSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    imgSrc: {
+        type: String,
+    },
+    cookTime: {
+        type: Number,
+        required: true
+    },
+    prepTime: {
+        type: Number,
+        required: true
+    },
+    Servings: {
+        type: Number,
+        required: true
+    },
+    cardDescription: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    description: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    sts_process: [
         {
-            type:mongoose.Schema.Types.ObjectId,
-            ref:"Review"
+            type: String,
+            required: true
+        }
+    ],
+    noOfIngredients: {
+        type: Number,
+        required: true
+    },
+    ingredients: [
+        {
+            type: String,
+            required: true
+        }
+    ],
+    nutrition: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Nutrition'
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    reviews: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Review"
         }
     ]
+}, { timestamps: true });
 
- 
-})
+recipeSchema.index({ title: 'text' });
 
-recipeSchema.index({title:'text'});
-
-recipeSchema.post('findOneAndDelete',async(doc)=>{
-    if(doc){
+recipeSchema.post('findOneAndDelete', async (doc) => {
+    if (doc) {
         await Review.deleteMany({
-            _id:{
-                $in:doc.reviews
-            }
-        })
-
+            _id: { $in: doc.reviews }
+        });
     }
-    
-})
-const recipe=mongoose.model('Recipe', recipeSchema)
+});
 
-module.exports=recipe;
+const Recipe = mongoose.model('Recipe', recipeSchema);
 
+module.exports = Recipe;
